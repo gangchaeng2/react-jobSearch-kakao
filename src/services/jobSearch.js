e06c898e467a2ba59c474c2373abafcd1ebbc1a8
gangchang <ktw6750@hanmail.net>
2017-12-28 00:49:05 +0900

    profileImg add and servicesJs create

---
+++ src/services/jobSearch.js
@@ -0,0 +1,104 @@
+import axios from 'axios';
+import fastXmlParser from 'fast-xml-parser';
+
+export function getJobListBySaramin(query) {
+    query = encodeURIComponent(query);
+    return axios.get(`https://cors-anywhere.herokuapp.com/http://api.saramin.co.kr/job-search?keywords=${query}`);
+}
+
+export function getJobListByIncuruit(query) {
+    query = encodeURIComponent(query);
+    return axios.get(`https://cors-anywhere.herokuapp.com/http://naeil.incruit.com/RSS?q=${query}&lo=&operTy=ANDOper`);
+}
+
+export function getJobList(query, site) {
+    let jobList = [];
+    switch(site) {
+        case '사람인':
+            jobList = getJobListBySaramin(query)
+                      .then(function(res){
+                          const jobJson = fastXmlParser.parse(res.data);
+                          console.log(jobJson);
+                          if(jobJson['job-search'].jobs.job.length > 0) {
+                              const jobList = jobJson['job-search'].jobs.job;
+                              let rtJobList = [];
+                
+                              jobList.forEach((job) => {
+                                  const jobObj = {
+                                      url: `http://www.saramin.co.kr/zf_user/jobs/relay/recruit-view?view_type=search&rec_idx=${job.id}&gz=1&t_ref=search&t_ref_content=generic&searchType=search`,
+                                      company: job.company.name,
+                                      detail: job.position
+                                  }
+                
+                                  rtJobList = rtJobList.concat(jobObj);
+                              });
+                              return rtJobList;
+                          } else {
+                              return null;
+                          }
+                      })
+                      .catch(function(error){
+                          return null;
+                      });
+            break;
+        case '인curuit':
+            jobList = getJobListByIncuruit(query)
+                      .then(function(res) {
+                          const jobJson = fastXmlParser.parse(res.data);
+                          console.log(jobJson);
+                          const jobList = jobJson.rss.channel.item;
+
+                          if(jobList.length > 0) {
+                            let rtJobList = [];
+              
+                            jobList.forEach((job) => {
+                                const jobObj = {
+                                    url: job.link,
+                                    company: job.company,
+                                    detail: {
+                                        title: job.title,
+                                        location: '',
+                                        'experience-level': '신입'
+                                    }
+                                }
+              
+                                rtJobList = rtJobList.concat(jobObj);
+                            });
+                            return rtJobList;
+                        } else {
+                            return null;
+                        }
+                      })
+                      .catch(function(error) {
+                          return null;  
+                      })
+            break;
+        default:
+          break;
+      }
+
+      return jobList;
+}
+
+export function ConvertSystemSourcetoHtml(str) {
+    console.log(str);
+    if(str !== undefined) {
+        str = str.replace("&lt;","<");
+        str = str.replace("&gt;",">");
+    } else {
+        str = "";
+    }
+
+    return str;
+}
+
+export function leadingZeros(n, digits) {
+    var zero = '';
+    n = n.toString();
+  
+    if (n.length < digits) {
+      for (let i = 0; i < digits - n.length; i++)
+        zero += '0';
+    }
+    return zero + n;
+}
