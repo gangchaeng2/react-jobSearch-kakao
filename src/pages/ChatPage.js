d963027ee28f5b3fb6c5209b79b7797516bb1dfa
gangchang <ktw6750@hanmail.net>
2017-12-28 00:48:43 +0900

    url param add

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,10 +1,24 @@
 import React from 'react';
+
+import saramin from '../img/saramin.png';
+import incuruit from '../img/incuruit.png';
 import { ChatContainer } from '../containers';
 
 const ChatPage = ({ match, post}) => {
+    let profileImg = null;
+    
+    if(match.params.site === '사람인') {
+        profileImg = saramin;
+    } else {
+        profileImg = incuruit;
+    }
+
     return (
         <div>
-            <ChatContainer />
+            <ChatContainer 
+                site={match.params.site}
+                profileImg={profileImg}
+            />
         </div>
     );
 };
