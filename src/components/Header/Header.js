57e8978a598e8b408f64a0c512763829d6bf01de
gangchang <ktw6750@hanmail.net>
2017-12-28 04:04:44 +0900

    Header component css modify

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,35 +7,37 @@ const cx = classNames.bind(styles);
 
 const Header = ({children}) => {
     const activeStyle = {
-        color: 'red'
+        color: 'white'
     };
     
     return (
-        <div>
-            <div className={cx('header')}>
+        <div className={cx('header')}>
+            <div className={cx('top-header')}>
                 <span className={cx('title')}>WorkTalk</span>
             </div>
-            <div className={cx('subheader')}>
-                <NavLink to="/" activeStyle={activeStyle}>
-                    <div className={cx('icon')}>
-                        <i className="fa fa-user"></i>
-                        <span className={cx('text')}>Friends</span>
-                    </div>
-                </NavLink>
+            <div className={cx('bottom-header')}>
+                <div className={cx('tab-bar')}>
+                    <NavLink exact to="/" activeStyle={activeStyle} className={cx('tab')}>
+                        <div className={cx('icon')}>
+                            <i className="fa fa-user"></i>
+                            <span className={cx('text')}>Friends</span>
+                        </div>
+                    </NavLink>
 
-                <NavLink exact to="/chatList" activeStyle={activeStyle}>
-                    <div className={cx('icon')}>
-                        <i className="fa fa-comment"></i>
-                        <span className={cx('text')}>Chat</span>
-                    </div>
-                </NavLink>
+                    <NavLink exact to="/chatList" activeStyle={activeStyle} className={cx('tab')}>
+                        <div className={cx('icon')}>
+                            <i className="fa fa-comment"></i>
+                            <span className={cx('text')}>Chat</span>
+                        </div>
+                    </NavLink>
 
-                <NavLink to="/more">
-                    <div className={cx('icon')}>
-                        <i className="fa fa-book"></i>
-                        <span className={cx('text')}>Channel</span>
-                    </div>
-                </NavLink>
+                    <NavLink to="/more" className={cx('tab')}>
+                        <div className={cx('icon')}>
+                            <i className="fa fa-book"></i>
+                            <span className={cx('text')}>Channel</span>
+                        </div>
+                    </NavLink>
+                </div>
             </div>
         </div>
     );
