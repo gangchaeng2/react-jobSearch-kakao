7eee5d41732e7d8e94c1ef0822f82d1fe38c681b
gangchang <ktw6750@hanmail.net>
2017-12-28 20:36:48 +0900

    SearchContainer FriendList arry add

--- src/containers/SearchContainer/SearchContainer.js
+++ src/containers/SearchContainer/SearchContainer.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 
+import saramin from '../../img/saramin.png';
+import incruit from '../../img/incruit.png';
 import { Search, Header } from '../../components';
 
 class SearchContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            friendList: [
+                {name: "사람인", introduce: "안녕하세요 사람인 입니다~", img: saramin},
+                {name: "인cruit", introduce: "안녕하세요 인cruit 입니다~", img: incruit}
+            ]
+        };
+    }
+
     render() {
+        const { friendList } = this.state;
+
         return (
             <div>
                 <Header />
-                <Search />
+                <Search 
+                    friendList={friendList}
+                />
             </div>
         );
     }
